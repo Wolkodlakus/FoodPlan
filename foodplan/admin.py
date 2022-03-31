8628fc66bd02a368e33488dcd384c0fe18f6eb57
Wolkodlakus <lesko68@list.ru>
2022-03-31 13:43:11 +0300

    Add models

--- foodplan/admin.py
+++ foodplan/admin.py
@@ -4,6 +4,10 @@ from django.urls import path
 # Register your models here.
 from.models import (
     Client,
+    Menu,
+    Subscription,
+    Allergy,
+    Promo,
 )
 
 @admin.register(Client)
@@ -12,4 +16,37 @@ class ClientAdmin(admin.ModelAdmin):
         'id',
         'name',
         'tg_chat_id',
+    )
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+    )
+
+@admin.register(Promo)
+class PromoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'code',
+        'valid_from',
+        'valid_to',
+        'discount',
+        'active'
+    )
+
+@admin.register(Allergy)
+class AllergyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name',
+    )
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'menu',
+        'portions',
+        'created_at', 'period',
     )
--- foodplan/models.py
+++ foodplan/models.py
@@ -1,16 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Client(models.Model):
-
     name = models.CharField(
         max_length=256,
-        verbose_name='Имя и фамилия клиента'
+        verbose_name='Имя и фамилия клиента',
+        blank=False,
+        null=False,
     )
     tg_chat_id = models.PositiveIntegerField(
         verbose_name='Чат id клиента в Телеграм',
-        unique=True
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    client_phonenumber = models.CharField(
+        verbose_name='Номер клиента',
+        max_length=20,
+        blank=False,
+        null=False,
+        default='0'
     )
 
     def __str__(self):
@@ -19,3 +31,114 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
+
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название Меню',
+        blank=False,
+        null=False,
+    )
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+
+class Allergy(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название аллергии',
+        blank=False,
+        null=False,
+        default="Нет"
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Аллергия'
+        verbose_name_plural = 'Аллергии'
+
+class Subscription(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название подписки',
+        blank=False,
+        null=False,
+    )
+    menu = models.ForeignKey(
+        Menu,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False
+    )
+    portions = models.PositiveIntegerField(
+        verbose_name='Количество порций',
+        blank=False,
+        null=False,
+        default=1,
+    )
+    allergies = models.ManyToManyField(Allergy)
+    created_at = models.DateTimeField(
+        verbose_name='Когда создана подписка',
+        default=timezone.now,
+        blank=False,
+        null=False,
+    )
+    period = models.IntegerField(
+        verbose_name='Период подписки',
+        blank=False,
+        null=False,
+        default=1,
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+class Promo(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название промокода',
+        blank=False,
+        null=False,
+    )
+    code = models.CharField(
+        max_length=256,
+        verbose_name='Промокод',
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    valid_from = models.DateTimeField(
+        verbose_name='С какой даты применим',
+        default=timezone.now,
+        blank=False,
+        null=False,
+    )
+    valid_to = models.DateTimeField(
+        verbose_name='По какую дату действует',
+        default=timezone.now,
+        blank=True,
+        null=True,
+    )
+    discount = models.IntegerField(
+        verbose_name='Размер скидки в процентах',
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        blank=False,
+        null=False,
+    )
+    active = models.BooleanField(
+        verbose_name='Активность промокода',
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = 'Промокод'
+        verbose_name_plural = 'Промокоды'
